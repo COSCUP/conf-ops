30d24fc09717580fc868f9db0984540bd6b9287a
yoyo930021 <yoyo930021@gmail.com>
2024-03-29 20:30:22 +0800

    chore: refactor config

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,13 +1,15 @@
 #/bin/bash
 
 db_pwd=$(openssl rand -base64 16 | tr -dc 'a-zA-Z0-9' | head -c 16)
+local_db_url="mysql://root:$db_pwd@127.0.0.1/conf-ops"
+secret_key=$(openssl rand -base64 32)
 
 docker run --name conf-ops-dev-mysql -e MYSQL_ROOT_PASSWORD=$db_pwd -p 3306:3306 -d mysql:8.0.36
 
 cat <<EOF > ./scripts/env.sh
 #/bin/bash
 
-export DATABASE_URL="mysql://root:$db_pwd@localhost/conf-ops"
+export DATABASE_URL="$local_db_url"
 export RUSTFLAGS="-L/opt/homebrew/opt/mysql-client@8.0/lib"
 EOF
 
@@ -15,13 +17,10 @@ chmod +x ./scripts/env.sh
 
 source ./scripts/env.sh
 
-cp ./Rocket.toml.example ./Rocket.toml
-
-cat <<EOF >> ./Rocket.toml
-
-[default.databases.main_db]
-url = "mysql://root:$db_pwd@localhost/conf-ops"
-EOF
+config=$(cat "./Rocket.toml.example")
+config=${config/<db_url>/$local_db_url}
+config=${config/<secret_key>/$secret_key}
+echo "$config" > Rocket.toml
 
 mkdir -p public
 
